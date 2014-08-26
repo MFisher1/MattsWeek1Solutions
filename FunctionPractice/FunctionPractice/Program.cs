f7f4ecf2a801ef155b41eccba34011c61b06cc5c
SeedPaths-User <lessdismissive@email.com>
2014-08-26 16:46:51 -0600

    Week1Home

---
+++ FunctionPractice/FunctionPractice/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //-Start of program
+
+            //Variables
+             
+            //--Counter
+            int c;
+
+
+            //Call HelloWorld function
+            HelloWolrd();
+
+            //Call Greeting function
+            Greeting("Matt");
+
+            //Call DoubleIt Function
+            int myAgeDoubled = DoubleIt(17);
+          
+            Console.WriteLine("My age doubled is " + myAgeDoubled);
+           
+            Console.WriteLine("Double my age doubled is " + DoubleIt(myAgeDoubled));
+
+            //Call Multiply Function
+            Console.WriteLine(Multiply(myAgeDoubled, 4));
+
+            //Loop from 1-10 that triples each number
+            for (int i = 1; i <= 10; i = i + 1)
+            {
+                Console.WriteLine(Multiply(i,3));
+            }
+
+            //Keep console window open
+            Console.ReadKey();
+
+         //-End of program
+          } 
+
+        //Functions
+        static void HelloWolrd()
+        {
+            Console.WriteLine("Hello World!!!:)");
+        }
+
+        static void Greeting(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+
+        static int DoubleIt(int num)
+        {
+            return num * 2;
+        }
+
+        static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        //-End of functions
+    }
+}
---
+++ Loop Practice/Loop Practice/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //-Start of program
+    
+            //Variables
+
+            //Call WhileTest function
+            WhileTest();
+
+            //Call CoinToss
+            CoinToss(100);
+
+            //Call FlipForHeads
+            FlipForHeads(48490489);
+
+            //Keep Console Open 
+            Console.ReadKey();
+
+        //-End of program
+        }
+
+        
+        //Functions
+        static void WhileTest()
+        {
+            Console.WriteLine("While Loop Test");
+            //While Loop From 1-10WhileTest()
+        
+
+            // declare incrementor
+            int i = 1;
+            
+            //define the while loop condition
+            while(i <= 10)
+            {
+                Console.WriteLine(i);
+                //increment i
+                i = i + 1;
+            }
+        }
+
+        static void CoinToss(int flips)
+        {
+            //Heads or tails
+            int headsCount = 0;
+            int tailsCount = 0;
+
+            //Rand num generator
+            Random rng = new Random();
+
+            for (int i = 1; i <= flips; i++)
+            {
+                //Flip the coin
+                int flip = rng.Next(0,2);
+
+                if (flip == 0)
+                {
+                    tailsCount++;
+                }
+                else
+                {
+                    headsCount++;
+                }
+            }
+            Console.WriteLine("Coin fliped " + flips + " times");
+            Console.WriteLine("Number of heads: " + headsCount);
+            Console.WriteLine("Number of tails " + tailsCount);
+        }
+
+        static void FlipForHeads(int numHeads)
+        {
+            //counts how many heads have been flipped
+            int headsFlipped = 0;
+
+            //incrementor and # of flips
+            int flips = 0;
+            
+            //random number generator
+            Random rng = new Random();
+            
+
+            //while loop to flip coins until desired num of heads is reached
+            
+            while (flips <= numHeads)
+            {
+                if (rng.Next(0,2) == 1)
+                {
+                    headsFlipped = headsFlipped + 1;
+                }
+                flips = flips + 1;
+            }
+            Console.WriteLine("It took " + flips + " flips to get " + headsFlipped + " heads.");
+        }
+        //-End of Functions
+    }
+}
