1bcb2548fdedb6391e115d95f2e8428181d9dca3
SeedPaths-User <lessdismissive@email.com>
2014-09-14 22:10:32 -0600

    Combat Sim V.2 & Week 3 coding challenge

--- Combat Simulator/Combat Simulator/Program.cs
+++ Combat Simulator/Combat Simulator/Program.cs
@@ -10,34 +10,28 @@ namespace Combat_Simulator
     {
         static void Main(string[] args)
         {
-            //Timer
-            var Timer = new System.Timers.Timer(2000);
+            
+           //Game Start
+            Game();
 
-            //Variables
-            //General
+           
+        }
+        //End of Main Function
+
+        //Functions
+        
+        //Game Start Function
+        static void Game()
+        {
+            //General Game Variables
             int round = 1;
             int turn = 1;
-            //Heros stats
-            //-HP: health points
-            int hp = 100;
-            int healthPotions = 4;
-            int skooma = 2;
-            int rage = 0;
-            int herosDamage = 0;
-            string name;
-            string herosMove;
-            bool skoomaUsed = false;
-            bool invalidMove = false;
-            //wh = warhammer: does 50 damage but only hits 70% of the time
-            //sw = sword: does 40 damage and hits 80% of the time
-            //dg = dagger: does 15 damage but hits 100% of the time
-            string weapon;
 
-            //enemies stats
-            int enemyHP = 200;
-            int enemyRage = 0;
-            int enemyDamage = 0;
-            string enemyMove;
+            
+           
+            //create player and enemy object
+            Player Hero = new Player();
+            Enemy Alduin = new Enemy();
 
             //Start
             ChangeText("Drem Yol Yok Dovakin.", "Greetings Dragonborn");
@@ -48,11 +42,11 @@ namespace Combat_Simulator
             Console.Clear();
             ChangeText("Fos Los Hin For Uld Hun", "What Is Your Name Mighty Hero?");
             Console.WriteLine();
-            name = Console.ReadLine();
+            Hero.Name = Console.ReadLine();
             Console.Clear();
 
             //Game Intro
-            Console.WriteLine(name + " you are the mighty Dovakin or DragonBorn. After training you're self to be the hero the world needs you to be(and much ale)"
+            Console.WriteLine(Hero.Name + " you are the mighty Dovakin or DragonBorn. After training you're self to be the hero the world needs you to be(and much ale)"
              + "You are ready to face the evil dragon Alduin. Though you a much smaller (and obviously not a huge fricken dragon). You have"
              + " The power of dragon shouts, courage and Skooma(Pretty much skyrim PCP)! During the fight you will have 4 abilities to choose from."
              + "If you're rage is high enough (goes up for every successfull attack) the dragon spirit is awoken in you and you can unleash,"
@@ -64,18 +58,18 @@ namespace Combat_Simulator
             Console.ReadKey();
             Console.Clear();
             //Pick weapon
-            Console.WriteLine("To start off your adventure, " + name + " you must first pick out your weaponry. Each has different pros and cons so chose wisely");
+            Console.WriteLine("To start off your adventure, " + Hero.Name + " you must first pick out your weaponry. Each has different pros and cons so chose wisely");
             Console.WriteLine("(Press the coresponding letter next to a weapon then press enter to chose it)");
             Console.WriteLine("wh = warhammer: does 40 damage but only hits 70% of the time");
             Console.WriteLine("sw = sword: does 30 damage and hits 80% of the time");
             Console.WriteLine("dg = dagger: does 15 damage but hits 100% of the time");
             //Figure out weapon variable
-            weapon = ChooseWeapon(Console.ReadLine());
+            Hero.Weapon = Hero.ChooseWeapon(Console.ReadLine());
             //If weapon is invalid loop until it isnt
-            while (weapon == "Invalid")
+            while (Hero.Weapon == "Invalid")
             {
                 Console.WriteLine("Invalid Choice, Pick Another Weapon");
-                weapon = ChooseWeapon(Console.ReadLine());
+                Hero.Weapon = Hero.ChooseWeapon(Console.ReadLine());
             }
             Console.Clear();
 
@@ -83,208 +77,68 @@ namespace Combat_Simulator
             System.Threading.Thread.Sleep(1000);
             Console.Clear();
 
-            //Game Start
-            while (hp != 0 && enemyHP != 0)
+            while (Hero.Health != 0 && Alduin.Health != 0)
             {
                 //Basic Game Stats
                 Console.WriteLine("Round: " + round);
-                Console.WriteLine("Alduin's Health: " + enemyHP);
-                Console.WriteLine("Alduins's Rage: " + enemyRage);
-                Console.WriteLine("Your Health: " + hp);
+                Console.WriteLine("Alduin's Health: " + Alduin.Health);
+                Console.WriteLine("Alduins's Rage: " + Alduin.Health);
+                Console.WriteLine("Your Health: " + Hero.Health);
+                Console.WriteLine("Your Rage: " + Hero.Rage);
                 Console.WriteLine();
+                Console.WriteLine("Press Enter to Start next round");
+                Console.ReadKey();
                 //Heros Turn
-                while(turn == 1 || invalidMove)
+                while (turn == 1 || Hero.InvalidMove)
                 {
-                    
-                        Console.Clear();
-                        //Pick move
-                        Console.WriteLine("Pick Your Move");
-                        Console.WriteLine();
-                        Console.WriteLine("S: Shout ");
-                        if (rage < 4)
-                        {
-                            Console.Write("(not enough rage!)");
-                        }
-                        Console.WriteLine("W: " + weapon);
-                        Console.WriteLine("H :Health Potion " + healthPotions);
-                        if (healthPotions <= 0)
-                        {
-                            Console.Write(" (out of potions)");
-                        }
-                        Console.WriteLine("SK: Skooma: " + skooma);
-                        if (skooma <= 0)
-                        {
-                            Console.Write(" (out of skooma)");
-                        }
-                        Console.WriteLine();
-                       
-                        herosMove = Console.ReadLine();
-                        Console.Clear();
-                        //If skooma was used last round add damage
-                        if (skoomaUsed == true)
-                        {
-                            herosDamage += 30;
-                        }
-                        //Execute move
 
-                        //Shout
-                        if (herosMove == "S" && rage < 4)
-                        {
-                            //If skooma was used last round add damage
-                            if (skoomaUsed == true)
-                            {
-                                herosDamage += 50;
-                            }
-                            herosDamage += 30 * rage;
-                            enemyHP -= herosDamage;
-                            rage = 0;
-                            Console.WriteLine("You're mighty shout did " + herosDamage + " damage!");
-                            skoomaUsed = false;
-                        }
-                        //Weapon
-                        else if (herosMove == "W")
-                        {
-                            //If skooma was used last round add damage
-                            if (skoomaUsed == true)
-                            {
-                                herosDamage += 30;
-                            }
-                            herosDamage += WeaponDamage(weapon);
-                            Console.WriteLine("Your " + weapon + " did " + herosDamage + " damage.");
-                            skoomaUsed = false;
-                        }
-                        //Health Potion
-                        else if (herosMove == "H" && healthPotions > 0)
-                        {
-                            hp += 30;
-                            Console.WriteLine("You healed yourself for 30 hp!");
-                            healthPotions--;
-                        }
-                        //Skooma
-                        else if (herosMove == "SK" && skooma > 0)
-                        {
-                            skoomaUsed = true;
-                            Console.WriteLine("Your Skooma gave you strength");
-                            skooma--;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Invalid Move");
-                            invalidMove = true;
-                        }
-                    
-                }
+                    //player move 
+                    Hero.ExecuteMove();
+                    Alduin.Health -= Hero.Damage;
                     //set enemy turn and reset heros damage
                     turn = 2;
-                    herosDamage = 0;
+                    Hero.Damage = 0;
                     System.Threading.Thread.Sleep(2000);
                     Console.Clear();
-
-                    //End of Heroes Turn
-                
-                    //Enemy Turn
-                    if (turn == 2)
-                    {
-                        enemyMove = EnemyMove(enemyRage);
-                        if (enemyMove == "fireball")
-                        {
-                            hp -= 30;
-                            Console.WriteLine("Enemy Fireball did 30 damage!");
-                        }
-                        else if (enemyMove == "shout")
-                        {
-                            hp -= 30 * enemyRage;
-                            Console.WriteLine("Enemy Shout did " + 30 * enemyRage + " damage!");
-                            enemyRage = 0;
-                        }
-                        else if (enemyMove == "bite")
-                        {
-                            hp -= 20;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Enemy Attack Failed!");
-                        }
-                        turn = 1;
-                    }
-                    //end of round
-                    round++;
-                    System.Threading.Thread.Sleep(1000);
-
-                
+                }
 
 
+                //End of Heroes Turn
+
+                //Enemy Turn
+                if (turn == 2)
+                {
+                    Alduin.ExecuteMove();
+                    Hero.Health -= Alduin.Damage;
+                    Alduin.Damage = 0;
+                    turn = 1;
+                }
+                //End of enemy turn
+
+                //end of round
+                round++;
+                System.Threading.Thread.Sleep(2000);
+                Console.Clear();
             }
 
             //End of game
-            if (enemyHP < 0 && hp < 0)
+            if (Alduin.Health <= 0 && Hero.Health <= 0)
             {
                 ChangeText("Lost grind yolus dinok nuz lost kuz hin paal voth hi! Nu hi fent velaaz ko mund do Sovengaurd ahrk joriin do keizaal fen dahmaan hi fah bok wah meyz!", "You have met a fierey death but have taken your foe with you! Now you shall feast in the halls of Sovengaurd and the people of skyrim will remember you for ages to come!");
             }
-            else if (enemyHP < 0)
+            else if (Alduin.Health <= 0)
             {
                 ChangeText("Dovahkiin! Hi lost krinaan Alduin, sav keizaal ahrk lask hinmaar unahzaal praal ko Sovengaurd!", "Congradulations Dragonborn! You have slain Alduin, saved skyrim and earned yourself an eternal seat in Sovengaurd!");
             }
-            else if (hp < 0)
-              {
-                  ChangeText("Not only have you been eaten alive, but you have left the world to be destroyed by Alduin!", "Not only have you been eaten alive, but you have left the world to be destroyed by Alduin!");  
-              }
+            else if (Hero.Health <= 0)
+            {
+                ChangeText("Not only have you been eaten alive, but you have left the world to be destroyed by Alduin!", "Not only have you been eaten alive, but you have left the world to be destroyed by Alduin!");
+            }
+            else
+            {
+                Console.WriteLine("Dude I have no idea what just happend..");
+            }
             Console.ReadKey();
-        }
-
-        //Functions
-        
-        //Weapon functions
-        //Choose weapon
-        static string ChooseWeapon(string weapon)
-        {
-            
-            if (weapon == "wh")
-            {
-                return "WarHammer";
-            }
-            else if (weapon == "sw")
-            {
-                return "Sword";
-            }
-            else if (weapon == "dg")
-            {
-                return "Dagger";
-            }
-            else
-            {
-                return "Invalid";
-            }
-        }
-
-        //Weapon Damage
-        static int WeaponDamage(string weapon)
-        {
-            //Chance
-            Random rnd = new Random();
-            int chance = rnd.Next(1, 10);
-            int damage = 0;
-
-            if (weapon == "WarHammer")
-            {
-                if (chance <= 7)
-                {
-                    damage = 40;
-                }
-            }
-            else if (weapon == "Sword")
-            {
-                if (chance <= 8)
-                {
-                    damage = 30;
-                }
-            }
-            else
-            {
-                damage = 15;
-            }
-            return damage;
-
         }
 
         //Change Text Function
@@ -307,54 +161,6 @@ namespace Combat_Simulator
             }
          }
 
-        //Enemy turn function
-        static string EnemyMove(int rage)
-        {
-            //enemy's ability's
-            //fireball - does a lot of damage but has a 70% chance of hitting
-            //shout - very strong ability but can only be used if Alduins "RAGE" is full, also has a 70% chance of hitting
-            //bite-week attack but always hits
-            
-            //rand int to decide which attack is used
-            Random rnd = new Random();
-            int move = rnd.Next(1, 4);
-            int chance = rnd.Next(1, 10);
-            //shout
-            if (rage > 10 && move == 3)
-            {
-                //70% chance of hitting
-                if (chance <= 7)
-                {
-                    return "shout";
-                }
-                    //attack failed
-                else
-                {
-                    return "failled";
-                }
-                
-            }
-            // fireball
-            else if (move == 1)
-            {
-                //70% chance of hitting
-                if (chance <= 7)
-                {
-                    return "fireball";
-                }
-                //attack failled
-                else
-                {
-                    return "failled";
-                }
-            }
-            //bite
-            else
-            {
-                return "bite";
-            }
-
-
-        }
+       
     }
 }
---
+++ Week3CodeChallenge/Week3CodeChallenge/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3CodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            maxPrime(1001);
+            Console.WriteLine(FibonacciSequencer(100000000));
+            Console.ReadKey();
+        }
+
+        //Functions
+
+        //Max Prime Function
+        //Loops until input max is reached, if i is prime print x, if not print i value.
+        static void maxPrime(int max)
+        {
+            //variables
+            //number being checked for being a prime
+            int i = 1;
+            //number that checks if i is a prime
+            int j = 1;
+            //counts how many primes have been recorded
+            int prime = 1;
+            List<int> output = new List<int>();
+            
+
+            //Figure out if prime
+            //Loop numbers until prime(prime counter) = max, 
+            //see if number i is divisible by anything but itself and 1
+
+             for (j = 1; prime <= max + 1; j++)
+                {
+                    //If i is not prime
+                    if (i % j == 0 && j != i && i != 1)
+                    {
+                        j = 1;
+                        i++;
+                    }
+                    //i must be prime, add i to output and increase prime counter
+                    else if (j == i)
+                    {
+                        output.Add(i);
+                        j = 1;
+                        i++;
+                        prime++;
+                    }
+
+                }
+            
+            
+            //write the last item in output to console
+            Console.WriteLine("The " + max + numSuffix(max) + " prime is " + output.Last());
+
+        }
+        //end of max prime
+          
+        //Even FibonacciSequencer
+        //Adds values of all fibonacci numbers up to max value
+        //figure out sequence by adding the two fibonacci numbers before it, starting with 1 and 2.
+        static int FibonacciSequencer(int maxValue)
+        {
+            //variables
+            //current fibonncaci # and previous #
+            int currentFib = 2;
+            int prevFib = 1;
+            int output = 0;
+            List<int> evenFib = new List<int>();
+            List<int> fibNumbs = new List<int>(1);
+           //loop to find fibonnaci #'s up to max
+            while (currentFib < maxValue)
+            {
+                
+                currentFib = prevFib + currentFib;
+                if (currentFib <= maxValue)
+                {
+                    fibNumbs.Add(currentFib);
+                }
+                prevFib = fibNumbs[fibNumbs.Count - 1];
+            }
+            //add all even fib #'s to a list then add them together to get the output
+            evenFib = fibNumbs.Where(x => x % 2 == 0).ToList();
+            foreach (var item in evenFib)
+            {
+                output = output + item;
+            }
+            return output;
+        }
+
+
+        //figure out the suffix of a number
+        static string numSuffix(int num)
+        {
+            string stringNum = num.ToString();
+            if (stringNum.Last().Equals('1'))
+            {
+                return "st";
+            }
+            else if (stringNum.Last().Equals('2'))
+            {
+                return "nd";
+            }
+            else if (stringNum.Last().Equals('3'))
+            {
+                return "rd";
+            }
+            else
+            {
+                return "th";
+            }
+            
+        }
+        //End of numSuffix
+     
+        //End of Functions
+    }
+}
