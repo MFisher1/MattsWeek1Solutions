1ad0f8a15b9cc690682f2b3554d4617b342d4608
SeedPaths-User <lessdismissive@email.com>
2014-09-29 12:34:30 -0600

    ds

--- Portfolio/Portfolio/Controllers/ContactController.cs
+++ Portfolio/Portfolio/Controllers/ContactController.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Portfolio.Controllers
+namespace MvcPractice.Controllers
 {
     public class ContactController : Controller
     {
         //
         // GET: /Contact/
-
+        [HttpGet]
         public ActionResult Index()
+        {
+            return View(new Models.ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Models.ContactForm contactForm)
+        {
+            //create a connection database
+            Models.ContactForm db = new Models.ContactForm();
+            //Add contact info to database
+            db.ContactForm.Add(contactForm);
+            //save changes
+            db.SaveChanges();
+            //Kick user to thank you page
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult ThankYou()
         {
             return View();
         }
